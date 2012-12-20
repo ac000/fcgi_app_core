47d7082b4857fbbe9369d3692cc7271e2d08e4b5
Andrew Clayton <andrew@opentechlabs.co.uk>
2012-12-20 23:46:12 +0000

    app: audit: Pad the microseconds out in log_login()
    In log_login() we log a timestamp in the form seconds.microseconds since
    the epoch in the utmp table.
    
    This was being done with the following format string
    
        %ld.%ld
    
    Change this to
    
        %ld.%06ld
    
    So 1356000609.19697 would now be stored as 1356000609.019697
    
    This matches the rest of the timestamps.
    
    Signed-off-by: Andrew Clayton <andrew@opentechlabs.co.uk>

--- src/audit.c
+++ src/audit.c
@@ -78,7 +78,7 @@ unsigned long long log_login(void)
 	sid = strtoull(row[0], NULL, 10) + 1;
 
 	/* Divide tv_nsec by 1000 to get a rough microseconds value */
-	sql_query("INSERT INTO utmp VALUES (%ld.%ld, %u, '%s', '%s', '%s', "
+	sql_query("INSERT INTO utmp VALUES (%ld.%06ld, %u, '%s', '%s', '%s', "
 			"%llu)",
 			login_at.tv_sec, login_at.tv_nsec / NS_USEC,
 			uid, username, ip_addr, hostname, sid);
