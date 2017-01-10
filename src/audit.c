ac000/fcgi_app_core

commit a8c24a68550a5d0db899250b3197c0deb1741f90

app: Fix potentially slow logins
When a user logs in we do a reverse DNS lookup of the users IP address
to get the hostname. We store both the IP address and hostname (if any)
of the requesting client in the utmp table. This is used in the 'Last
login' line we display. Also having both the IP address _and_ hostname
provides a better audit trail.

Normally this should only take a fraction of a second. However there are
cases where we get a 2(SERVFAIL) after a timeout. If there was just no
PTR record then we'd get a 3(NXDOMAIN) pretty quickly.

Seeing as there is no need to delay the login just to get the hostname,
we can do the IP->hostname lookup in a thread which will do the lookup
and update the utmp table accordingly based on the clients session id.

We move the hostname lookup out of log_login() and into a thread routine
log_utmp_host() which is created from log_login. This allows the login
to complete right away and the utmp table is updated asynchronously.

Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

src/audit.c
@@ -136,6 +136,54 @@ out3:
 	return login_ok;
 }
 
+struct utmp_info {
+	char ip[INET6_ADDRSTRLEN + 1];	/* IP address of client */
+	unsigned long long sid;		/* Session ID of client */
+};
+
+/*
+ * Thread to lookup the hostname of the client IP address and update
+ * the umtp table at login.
+ *
+ * This is done in a separate thread as it can sometimes take a number
+ * of seconds to complete and there's no need to hold up the login for it.
+ */
+static void *log_utmp_host(void *arg)
+{
+	char *hostname;
+	char host[NI_MAXHOST] = "\0";
+	struct sockaddr_in addr4;
+	struct sockaddr_in6 addr6;
+	struct sockaddr *addr = (struct sockaddr *)&addr4;
+	struct utmp_info *ui = (struct utmp_info *)arg;
+	socklen_t addr_len = sizeof(addr4);
+	MYSQL *db;
+
+	if (!strchr(ui->ip, ':')) {
+		/* IPv4 */
+		inet_pton(AF_INET, ui->ip, &addr4.sin_addr);
+		addr4.sin_family = AF_INET;
+	} else {
+		/* IPv6 */
+		inet_pton(AF_INET6, ui->ip, &addr6.sin6_addr);
+		addr6.sin6_family = AF_INET6;
+
+		addr = (struct sockaddr *)&addr6;
+		addr_len = sizeof(addr6);
+	}
+	getnameinfo(addr, addr_len, host, NI_MAXHOST, NULL, 0, 0);
+
+	db = db_conn();
+	hostname = make_mysql_safe_string(host);
+	sql_query("UPDATE utmp SET hostname = '%s' WHERE sid = %llu",
+			hostname, ui->sid);
+	mysql_close(db);
+	free(hostname);
+	free(ui);
+
+	return 0;
+}
+
 /*
  * Add a login entry to the utmp table.
  *
@@ -145,37 +193,19 @@ out3:
 unsigned long long log_login(void)
 {
 	char *username;
-	char *hostname;
 	char *ip_addr;
-	char host[NI_MAXHOST] = "\0";
 	struct timespec login_at;
-	struct sockaddr_in addr4;
-	struct sockaddr_in6 addr6;
-	struct sockaddr *addr = (struct sockaddr *)&addr4;
+	struct utmp_info *ui;
 	unsigned long long sid;
 	unsigned int uid;
-	socklen_t addr_len = sizeof(addr4);
 	MYSQL_RES *res;
 	MYSQL_ROW row;
+	pthread_t tid;
+	pthread_attr_t attr;
 
 	clock_gettime(CLOCK_REALTIME, &login_at);
 
-	if (!strchr(env_vars.remote_addr, ':')) {
-		/* IPv4 */
-		inet_pton(AF_INET, env_vars.remote_addr, &addr4.sin_addr);
-		addr4.sin_family = AF_INET;
-	} else {
-		/* IPv6 */
-		inet_pton(AF_INET6, env_vars.remote_addr, &addr6.sin6_addr);
-		addr6.sin6_family = AF_INET6;
-
-		addr = (struct sockaddr *)&addr6;
-		addr_len = sizeof(addr6);
-	}
-	getnameinfo(addr, addr_len, host, NI_MAXHOST, NULL, 0, 0);
-
 	username = make_mysql_safe_string(get_var(qvars, "username"));
-	hostname = make_mysql_safe_string(host);
 	ip_addr = make_mysql_safe_string(env_vars.remote_addr);
 	res = sql_query("SELECT uid FROM passwd WHERE username = '%s'",
 			username);
@@ -191,16 +221,27 @@ unsigned long long log_login(void)
 	sid = strtoull(row[0], NULL, 10) + 1;
 
 	/* Divide tv_nsec by 1000 to get a rough microseconds value */
-	sql_query("INSERT INTO utmp VALUES (%ld.%06ld, %u, '%s', '%s', '%s', "
+	sql_query("INSERT INTO utmp VALUES (%ld.%06ld, %u, '%s', '%s', '', "
 			"%llu)",
 			login_at.tv_sec, login_at.tv_nsec / NS_USEC,
-			uid, username, ip_addr, hostname, sid);
+			uid, username, ip_addr, sid);
 	sql_query("UNLOCK TABLES");
 
 	mysql_free_result(res);
 	free(username);
-	free(hostname);
 	free(ip_addr);
+
+	/*
+	 * ui is free'd in the log_utmp_host thread as it will need to
+	 * exist beyond the life of this function.
+	 */
+	ui = malloc(sizeof(struct utmp_info));
+	snprintf(ui->ip, sizeof(ui->ip), "%s", env_vars.remote_addr);
+	ui->sid = sid;
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+	pthread_create(&tid, &attr, log_utmp_host, (void *)ui);
+	pthread_attr_destroy(&attr);
 
 	return sid;
 }

src/Makefile
@@ -1,7 +1,7 @@
 CC=gcc
 CFLAGS=-Wall -std=c99 -O2 -g -Wp,-D_FORTIFY_SOURCE=2 -D_FILE_OFFSET_BITS=64 -D_HAVE_FCGX -fexceptions -fstack-protector --param=ssp-buffer-size=4 -fPIE
 LDFLAGS=-Wl,-z,now -pie
-LIBS=-L./libctemplate `pkg-config --libs glib-2.0` `pkg-config --libs gmime-2.0` `mysql_config --libs` -lm -lrt -lcrypt -lfcgi -lmhash -ltokyocabinet -lctemplate-fcgx
+LIBS=-L./libctemplate `pkg-config --libs glib-2.0` `pkg-config --libs gmime-2.0` `mysql_config --libs` -lm -lrt -lcrypt -lfcgi -lmhash -ltokyocabinet -lctemplate-fcgx -pthread
 INCS=-I./libctemplate `pkg-config --cflags glib-2.0` `pkg-config --cflags gmime-2.0` `mysql_config --cflags`
 
 objects = app.o get_config.o url_handlers.o utils.o db.o audit.o csrf.o

commit ddd96ab7b5ec1bff83bfbf6c18979b0a07fdfd2c

audit: Fix a segfault when doing name lookups
Commit a8c24a6 ("app: Fix potentially slow logins") introduced a fix for
doing name lookups on the clients IP address at login time where this
could sometimes take a few seconds delaying login. That fix moved the
name lookup into its own thread.

However that introduced a bug. The thread was messing with the global
MYSQL *conn handle.

We need to use a db connection that is private to the
log_utmp_host() thread. To do this we create a new db_conn_local()
function that wraps db_conn() which itself has some adjustments, that
will provide a private db connection and not touch the global MYSQL
*conn variable.

We also do a similar thing with new sql_squeryl() and
make_mysql_safe_stringl() functions that take a MYSQL * as their first
parameter rather than operating on the global connection.

The advantage of providing these wrapper functions and making some minor
changes to the underlying functions that actually do the work, means all
the existing sql_squery() and make_mysql_safe_string() calls remain
unaffected.

Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

src/audit.c
@@ -314,9 +314,9 @@ static void *log_utmp_host(void *arg)
 	getnameinfo(res->ai_addr, res->ai_addrlen, host, NI_MAXHOST, NULL, 0,
 			0);
 
-	db = db_conn();
-	hostname = make_mysql_safe_string(host);
-	sql_query("UPDATE utmp SET hostname = '%s' WHERE sid = %llu",
+	db = db_conn_local();
+	hostname = make_mysql_safe_stringl(db, host);
+	sql_queryl(db, "UPDATE utmp SET hostname = '%s' WHERE sid = %llu",
 			hostname, ui->sid);
 	mysql_close(db);
 	free(hostname);

src/common.h
@@ -4,7 +4,7 @@
  * Copyright (C) 2012 - 2013	OpenTech Labs
  *				Andrew Clayton <andrew@digital-domain.net>
  *
- *		 2014		Andrew Clayton <andrew@digital-domain.net>
+ *		 2014, 2016	Andrew Clayton <andrew@digital-domain.net>
  *
  * This software is released under the MIT License (MIT-LICENSE.txt)
  * and the GNU Affero General Public License version 3 (AGPL-3.0.txt)
@@ -113,11 +113,28 @@
  * Given a string it will return a string, that must be free'd, that is safe
  * to pass to mysql.
  */
+static inline char *__make_mysql_safe_string(MYSQL *dbconn, const char *string)
+{
+	char *safe = malloc(strlen(string)*2 + 1);
+
+	mysql_real_escape_string(dbconn, safe, string, strlen(string));
+	return safe;
+}
+
+/*
+ * Warppaer around __make_mysql_safe_string() using the global db connection.
+ */
 static inline char *make_mysql_safe_string(const char *string)
 {
-	char *safe = malloc(strlen(string) * 2 + 1);
-	mysql_real_escape_string(conn, safe, string, strlen(string));
-	return safe;
+	return __make_mysql_safe_string(conn, string);
+}
+
+/*
+ * Warppaer around __make_mysql_safe_string() using the local db connection.
+ */
+static inline char *make_mysql_safe_stringl(MYSQL *dbconn, const char *string)
+{
+	return __make_mysql_safe_string(dbconn, string);
 }
 
 /*

src/db.c
@@ -4,7 +4,7 @@
  * Copyright (C) 2012		OpenTech Labs
  *				Andrew Clayton <andrew@digital-domain.net>
  *
- *		 2014		Andrew Clayton <andrew@digital-domain.net>
+ *		 2014, 2016	Andrew Clayton <andrew@digital-domain.net>
  *
  * This software is released under the MIT License (MIT-LICENSE.txt)
  * and the GNU Affero General Public License version 3 (AGPL-3.0.txt)
@@ -26,6 +26,8 @@
 /* Global MySQL connection handle */
 MYSQL *conn;
 
+enum { DB_CONN_GLOBAL, DB_CONN_LOCAL };
+
 char *db_host = "localhost";
 char *db_socket_name = NULL;
 unsigned int db_port_num = 3306;
@@ -34,8 +36,9 @@ unsigned int db_flags = 0;
 /*
  * Opens up a MySQL connection and returns the connection handle.
  */
-MYSQL *db_conn(void)
+static MYSQL *__db_conn(int db_conn_type)
 {
+	MYSQL *dbc;
 	MYSQL *ret;
 
 	if (MULTI_TENANT) {
@@ -47,22 +50,42 @@ MYSQL *db_conn(void)
 		free(db_name);
 		db_name = strdup(db);
 	}
-	conn = mysql_init(NULL);
-	ret = mysql_real_connect(conn, DB_HOST, DB_USER, DB_PASS, DB_NAME,
+
+	dbc = mysql_init(NULL);
+	ret = mysql_real_connect(dbc, DB_HOST, DB_USER, DB_PASS, DB_NAME,
 			DB_PORT_NUM, DB_SOCKET_NAME, DB_FLAGS);
 
 	if (!ret) {
 		d_fprintf(error_log, "Failed to connect to database. Error: "
-				"%s\n", mysql_error(conn));
-		switch (mysql_errno(conn)) {
+				"%s\n", mysql_error(dbc));
+		switch (mysql_errno(dbc)) {
 		case ER_BAD_DB_ERROR:	/* unknown database */
 			send_page("templates/invalid.tmpl");
 			break;
 		}
-		conn = NULL;
+		dbc = NULL;
 	}
 
-	return conn;
+	if (db_conn_type == DB_CONN_GLOBAL)
+		conn = dbc;
+
+	return dbc;
+}
+
+/*
+ * Wrapper around __db_conn() to open a new global db connection.
+ */
+MYSQL *db_conn(void)
+{
+	return __db_conn(DB_CONN_GLOBAL);
+}
+
+/*
+ * Wrapper around __db_conn() to open a new local db connection.
+ */
+MYSQL *db_conn_local(void)
+{
+	return __db_conn(DB_CONN_LOCAL);
 }
 
 /*
@@ -76,7 +99,7 @@ MYSQL *db_conn(void)
  * This function will either return a result set or NULL. Note that some
  * queries don't return result sets by design.
  */
-MYSQL_RES *__sql_query(const char *func, const char *fmt, ...)
+MYSQL_RES *__sql_query(MYSQL *dbconn, const char *func, const char *fmt, ...)
 {
 	va_list args;
 	char sql[SQL_MAX];

src/db.h
@@ -4,6 +4,8 @@
  * Copyright (C) 2012 - 2013	OpenTech Labs
  *				Andrew Clayton <andrew@digital-domain.net>
  *
+ * 		 2016		Andrew Clayton <andrew@digital-domain.net>
+ *
  * This software is released under the MIT License (MIT-LICENSE.txt)
  * and the GNU Affero General Public License version 3 (AGPL-3.0.txt)
  */
@@ -29,11 +31,18 @@
  * sql log.
  */
 #define sql_query(fmt, ...) \
-	__sql_query((const char *)__func__, fmt, ##__VA_ARGS__)
+	__sql_query(NULL, (const char *)__func__, fmt, ##__VA_ARGS__)
+
+/*
+ * Same as the above, but takes a MYSQL * for the connection to operate on.
+ */
+#define sql_queryl(dbconn, fmt, ...) \
+	__sql_query(dbconn, (const char *)__func__, fmt, ##__VA_ARGS__)
 
 extern MYSQL *conn;
 
 MYSQL *db_conn(void);
-MYSQL_RES *__sql_query(const char *func, const char *fmt, ...);
+MYSQL *db_conn_local(void);
+MYSQL_RES *__sql_query(MYSQL *dbconn, const char *func, const char *fmt, ...);
 
 #endif /* _DB_H_ */