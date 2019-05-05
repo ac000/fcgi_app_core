ac000/fcgi_app_core

commit ddd96ab7b5ec1bff83bfbf6c18979b0a07fdfd2c

audit: Fix a segfault when doing name lookups
Commit a8c24a6 ("app: Fix potentially slow logins") introduced a fix for
doing name lookups on the clients IP address at login time where this
could sometimes take a few seconds delaying login. That fix moved the
name lookup into its own thread.

However that introduced a bug. The thread was messing with the global
MYSQL *conn handle.

We need to use a db connection that is private to the
log_utmp_host() thread. To do this we create a new db_conn_local()
function that wraps db_conn() which itself has some adjustments, that
will provide a private db connection and not touch the global MYSQL
*conn variable.

We also do a similar thing with new sql_squeryl() and
make_mysql_safe_stringl() functions that take a MYSQL * as their first
parameter rather than operating on the global connection.

The advantage of providing these wrapper functions and making some minor
changes to the underlying functions that actually do the work, means all
the existing sql_squery() and make_mysql_safe_string() calls remain
unaffected.

Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

src/db.c
@@ -4,7 +4,7 @@
  * Copyright (C) 2012		OpenTech Labs
  *				Andrew Clayton <andrew@digital-domain.net>
  *
- *		 2014		Andrew Clayton <andrew@digital-domain.net>
+ *		 2014, 2016	Andrew Clayton <andrew@digital-domain.net>
  *
  * This software is released under the MIT License (MIT-LICENSE.txt)
  * and the GNU Affero General Public License version 3 (AGPL-3.0.txt)
@@ -26,6 +26,8 @@
 /* Global MySQL connection handle */
 MYSQL *conn;
 
+enum { DB_CONN_GLOBAL, DB_CONN_LOCAL };
+
 char *db_host = "localhost";
 char *db_socket_name = NULL;
 unsigned int db_port_num = 3306;
@@ -34,8 +36,9 @@ unsigned int db_flags = 0;
 /*
  * Opens up a MySQL connection and returns the connection handle.
  */
-MYSQL *db_conn(void)
+static MYSQL *__db_conn(int db_conn_type)
 {
+	MYSQL *dbc;
 	MYSQL *ret;
 
 	if (MULTI_TENANT) {
@@ -47,22 +50,42 @@ MYSQL *db_conn(void)
 		free(db_name);
 		db_name = strdup(db);
 	}
-	conn = mysql_init(NULL);
-	ret = mysql_real_connect(conn, DB_HOST, DB_USER, DB_PASS, DB_NAME,
+
+	dbc = mysql_init(NULL);
+	ret = mysql_real_connect(dbc, DB_HOST, DB_USER, DB_PASS, DB_NAME,
 			DB_PORT_NUM, DB_SOCKET_NAME, DB_FLAGS);
 
 	if (!ret) {
 		d_fprintf(error_log, "Failed to connect to database. Error: "
-				"%s\n", mysql_error(conn));
-		switch (mysql_errno(conn)) {
+				"%s\n", mysql_error(dbc));
+		switch (mysql_errno(dbc)) {
 		case ER_BAD_DB_ERROR:	/* unknown database */
 			send_page("templates/invalid.tmpl");
 			break;
 		}
-		conn = NULL;
+		dbc = NULL;
 	}
 
-	return conn;
+	if (db_conn_type == DB_CONN_GLOBAL)
+		conn = dbc;
+
+	return dbc;
+}
+
+/*
+ * Wrapper around __db_conn() to open a new global db connection.
+ */
+MYSQL *db_conn(void)
+{
+	return __db_conn(DB_CONN_GLOBAL);
+}
+
+/*
+ * Wrapper around __db_conn() to open a new local db connection.
+ */
+MYSQL *db_conn_local(void)
+{
+	return __db_conn(DB_CONN_LOCAL);
 }
 
 /*
@@ -76,7 +99,7 @@ MYSQL *db_conn(void)
  * This function will either return a result set or NULL. Note that some
  * queries don't return result sets by design.
  */
-MYSQL_RES *__sql_query(const char *func, const char *fmt, ...)
+MYSQL_RES *__sql_query(MYSQL *dbconn, const char *func, const char *fmt, ...)
 {
 	va_list args;
 	char sql[SQL_MAX];

src/audit.c
@@ -314,9 +314,9 @@ static void *log_utmp_host(void *arg)
 	getnameinfo(res->ai_addr, res->ai_addrlen, host, NI_MAXHOST, NULL, 0,
 			0);
 
-	db = db_conn();
-	hostname = make_mysql_safe_string(host);
-	sql_query("UPDATE utmp SET hostname = '%s' WHERE sid = %llu",
+	db = db_conn_local();
+	hostname = make_mysql_safe_stringl(db, host);
+	sql_queryl(db, "UPDATE utmp SET hostname = '%s' WHERE sid = %llu",
 			hostname, ui->sid);
 	mysql_close(db);
 	free(hostname);

src/common.h
@@ -4,7 +4,7 @@
  * Copyright (C) 2012 - 2013	OpenTech Labs
  *				Andrew Clayton <andrew@digital-domain.net>
  *
- *		 2014		Andrew Clayton <andrew@digital-domain.net>
+ *		 2014, 2016	Andrew Clayton <andrew@digital-domain.net>
  *
  * This software is released under the MIT License (MIT-LICENSE.txt)
  * and the GNU Affero General Public License version 3 (AGPL-3.0.txt)
@@ -113,11 +113,28 @@
  * Given a string it will return a string, that must be free'd, that is safe
  * to pass to mysql.
  */
+static inline char *__make_mysql_safe_string(MYSQL *dbconn, const char *string)
+{
+	char *safe = malloc(strlen(string)*2 + 1);
+
+	mysql_real_escape_string(dbconn, safe, string, strlen(string));
+	return safe;
+}
+
+/*
+ * Warppaer around __make_mysql_safe_string() using the global db connection.
+ */
 static inline char *make_mysql_safe_string(const char *string)
 {
-	char *safe = malloc(strlen(string) * 2 + 1);
-	mysql_real_escape_string(conn, safe, string, strlen(string));
-	return safe;
+	return __make_mysql_safe_string(conn, string);
+}
+
+/*
+ * Warppaer around __make_mysql_safe_string() using the local db connection.
+ */
+static inline char *make_mysql_safe_stringl(MYSQL *dbconn, const char *string)
+{
+	return __make_mysql_safe_string(dbconn, string);
 }
 
 /*

src/db.h
@@ -4,6 +4,8 @@
  * Copyright (C) 2012 - 2013	OpenTech Labs
  *				Andrew Clayton <andrew@digital-domain.net>
  *
+ * 		 2016		Andrew Clayton <andrew@digital-domain.net>
+ *
  * This software is released under the MIT License (MIT-LICENSE.txt)
  * and the GNU Affero General Public License version 3 (AGPL-3.0.txt)
  */
@@ -29,11 +31,18 @@
  * sql log.
  */
 #define sql_query(fmt, ...) \
-	__sql_query((const char *)__func__, fmt, ##__VA_ARGS__)
+	__sql_query(NULL, (const char *)__func__, fmt, ##__VA_ARGS__)
+
+/*
+ * Same as the above, but takes a MYSQL * for the connection to operate on.
+ */
+#define sql_queryl(dbconn, fmt, ...) \
+	__sql_query(dbconn, (const char *)__func__, fmt, ##__VA_ARGS__)
 
 extern MYSQL *conn;
 
 MYSQL *db_conn(void);
-MYSQL_RES *__sql_query(const char *func, const char *fmt, ...);
+MYSQL *db_conn_local(void);
+MYSQL_RES *__sql_query(MYSQL *dbconn, const char *func, const char *fmt, ...);
 
 #endif /* _DB_H_ */

commit 6133234b6d66314837b0bba0ac52fe8d2bfebe70

db: Fix some brokenness in __sql_query()
This was found with the following compiler warning (after adding -Wextra
to the compiler flags in commit f8b47ff ("Makefile: Compile with
-Wextra"))

  CC    db.o
db.c: In function ‘__sql_query’:
db.c:103:31: warning: unused parameter ‘dbconn’ [-Wunused-parameter]
  103 | MYSQL_RES *__sql_query(MYSQL *dbconn, const char *func, const char *fmt, ...)
      |                        ~~~~~~~^~~~~~

That stems from an earlier fix in commit ddd96ab ("audit: Fix a segfault
when doing name lookups"), that itself was ported over from
receiptomatic[0], Which resulted in changes to __sql_query() being
missed.

[0]: https://github.com/ac000/receiptomatic/commit/4404fc530ae5a3036808dd7db1165968c9be9880

Fixes: ddd96ab ("audit: Fix a segfault when doing name lookups")
Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

src/db.c
@@ -104,6 +104,7 @@ MYSQL_RES *__sql_query(MYSQL *dbconn, const char *func, const char *fmt, ...)
 	va_list args;
 	char sql[SQL_MAX];
 	int len;
+	MYSQL *dbc = conn;
 
 	va_start(args, fmt);
 	len = vsnprintf(sql, sizeof(sql), fmt, args);
@@ -122,6 +123,9 @@ MYSQL_RES *__sql_query(MYSQL *dbconn, const char *func, const char *fmt, ...)
 		fflush(sql_log);
 	}
 
-	mysql_real_query(conn, sql, len);
-	return mysql_store_result(conn);
+	if (dbconn)
+		dbc = dbconn;
+	mysql_real_query(dbc, sql, len);
+
+	return mysql_store_result(dbc);
 }